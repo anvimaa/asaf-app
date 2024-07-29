5fa084e758845d0c02a65b6a04e76725eb3652ca
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-07-29 18:42:06 +0100

    add header and sidebar components, and update Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import Header from '@/components/sections/header.svelte';
+	import SideBar from '@/components/sections/side-bar.svelte';
 </script>
 
-<slot></slot>
+<div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+	<SideBar />
+	<div class="flex flex-col">
+		<Header />
+		<main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+			<slot />
+		</main>
+	</div>
+</div>
