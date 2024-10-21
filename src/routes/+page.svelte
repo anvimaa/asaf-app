442f94a8433ceb5eba1d676757134e732c467367
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-10-21 22:53:55 +0100

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,48 @@
-<script lang="ts">
-	import Table from '@/components/elements/table/table.svelte';
-</script>
+<svelte:head>
+	<title>SGH - SADISSA</title>
+</svelte:head>
 
-<h1>Pagina inicial</h1>
-<a href="/dashboard">Painel</a>
+<!-- Cabeçalho -->
+<header class="bg-white shadow">
+	<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+		<h1 class="text-3xl font-bold text-gray-900">SGH - SADISSA</h1>
+	</div>
+</header>
+
+<!-- Seção Principal -->
+<main class=" h-screen bg-white">
+	<div class="relative overflow-hidden bg-white">
+		<div class="mx-auto max-w-7xl">
+			<div
+				class="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32"
+			>
+				<div class="mx-auto mt-10 max-w-7xl px-4 sm:px-6 lg:px-8">
+					<div class="sm:text-center lg:text-left">
+						<h2
+							class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
+						>
+							<span class="block">Simplifique</span>
+							<span class="block text-indigo-600">a Gestão Hospitalar</span>
+						</h2>
+						<p
+							class="mt-3 text-base text-gray-500 sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0"
+						>
+							Gerencie de forma eficiente os pacientes, funcionários e operações do hospital com o
+							nosso sistema completo de gestão.
+						</p>
+						<div class="mt-5 justify-center sm:mt-8 sm:flex lg:justify-start">
+							<div class="rounded-md shadow">
+								<a
+									href="/login"
+									class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:px-10 md:py-4 md:text-lg"
+								>
+									Começar
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
